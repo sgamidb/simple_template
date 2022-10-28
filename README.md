8bb63d7db0259a250915d61412c10e37b22ef99f
sgamidb <36730095+sgamidb@users.noreply.github.com>
2022-10-28 11:15:54 +0200

    chore: parse string to binary

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Sujet STeganographie GuyO
+# Sujet Steganographie GuyO
 
-Tasks WIP :
+
+## Business rule :
+Pour une chaine intégrer dans une image, après décodage avec l'image de base on doit retrouver la même chaîne de caractères.
+
+
+## Tâches : 
+- convertir une chaine de caractère en bit array et la reconvertir en string -> la chaine doit rester identique
+- lire une image / la transformer en bit array / l'enregistrer -> l'image doit rester identique
+- encodage d'une image en ajoutant de la donnée
+- decodade du message en comparant avec l'image source (au format bit array)
+
+
--- test/check.test.js
+++ test/check.test.js
@@ -1,5 +1,58 @@
-describe('', () => {
-    it('should failed', () => {
-        expect(false).toEqual(false);
+describe('convert a string to bit array', () => {
+    it.each([
+        ["A", "01000001"],
+        ["B", "01000010"],
+        ["C", "01000011"],
+        ["D", "01000100"],
+        ["E", "01000101"],
+        ["F", "01000110"],
+        ["G", "01000111"],
+        ["H", "01001000"],
+        ["I", "01001001"],
+        ["J", "01001010"],
+        ["AB", "0100000101000010"],
+    ])('should converted %s to expected %s', (word, bitArray) => {
+        const result = encodeString(word);
+
+        expect(result).toEqual(bitArray.split(''))
     })
 });
+
+describe('convert a bit array to bit string', () => {
+    it.each([
+        ["01000001", "A" ],
+        ["01000010", "B" ],
+        ["01000011", "C" ],
+        ["01000100", "D" ],
+        ["01000101", "E" ],
+        ["01000110", "F" ],
+        ["01000111", "G" ],
+        ["01001000", "H" ],
+        ["01001001", "I" ],
+        ["01001010", "J" ],
+        ["0100000101000010", "AB" ],
+    ])('should converted %s to expected %s', (bitArray, word) => {
+        const result = decodeToString(bitArray);
+
+        expect(result).toEqual(word)
+    })
+});
+
+function encodeString(word) {
+    return word.split('').map(letter=>letter.charCodeAt(0).toString(2).padStart(8, '0')).join('').split('');
+}
+
+function decodeToString(bitArray){
+    const arrByOctet = bitArray.match(/(\d{8})/g);
+    return String.fromCharCode(...arrByOctet.map(b=>parseInt(b, 2)));
+}
+
+
+/*
+function encodeString(word) {
+    const textEncoder = new TextEncoder();
+    const uint8array = textEncoder.encode(word);
+    return uint8array.map(v=>v.toString(2))
+}
+
+*/
