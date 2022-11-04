a9d9f4b6e746e831e9bcaf9a4383767043b0e214
sgamidb <36730095+sgamidb@users.noreply.github.com>
2022-11-04 11:12:48 +0100

    chore: implement retrieve image$

--- test/add-message.test.js
+++ test/add-message.test.js
@@ -1,48 +1,74 @@
 describe('encode message in image', () => {
   it('should add the encoding message in the image', () => {
-    const baseImage = [
-      [
-        [0, 0, 0], [0, 0, 0]
-      ],
-      [
-        [0, 0, 0], [0, 0, 0]
-      ]
-    ]
+    const baseImage = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     const message = '01000001'
 
     const imageWithMessage = addMessageToImage(baseImage, message)
-    console.table(imageWithMessage);
-    console.table(baseImage);
-    expect(imageWithMessage).toEqual([
-      [
-        [0, 1, 0], [0, 0, 0]
-      ],
-      [
-        [0, 1, 0], [0, 0, 0]
-      ]
-    ])
+
+    expect(imageWithMessage).toEqual([0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+  });
+
+  it('should throw an error if there is not engouhb byte to add the message', () => {
+    const baseImage = [0, 0, 0, 0, 0, 0]
+    const message = '01000001'
+
+    const callToAddMessageToImage = () => addMessageToImage(baseImage, message);
+
+    expect(callToAddMessageToImage).toThrowError("Image is too small");
   })
 })
 
+
+describe('decode message in image', () => {
+  it('should retrieve the message in the image', ()=> {
+    const expectedMessage = '01000001';
+    const baseImage = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const encodedImage = [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0];
+
+    const messageRetrieved = retrieveMessageInImage(baseImage, encodedImage );
+
+    expect(messageRetrieved).toEqual(expectedMessage);
+  });
+
+  it('should retrieve the message in the image', ()=> {
+    const expectedMessage = '01000001';
+    const baseImage = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const encodedImage = [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const messageRetrieved = retrieveMessageInImage(baseImage, encodedImage );
+
+    expect(messageRetrieved).toEqual(expectedMessage);
+  });
+
+  it('should throw when the image have not the same length', ()=> {
+    const baseImage = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const encodedImage = [0, 1, 0, 0, 0, 0, 0, 1, 0 ];
+
+    const callToRetrieveMLessageInImage = ()=>retrieveMessageInImage(baseImage, encodedImage);
+
+    expect(callToRetrieveMLessageInImage).toThrowError("Image have not the same length");
+  })
+});
+
 function addMessageToImage (baseImage, message) {
-  const bitArray = message.split('').map(v=>+v);
-  let bitArrayIndex = -1
-
-  for (let i = 0; i < baseImage.length; i += 1) {
-    for (let j = 0; j < baseImage[i].length; j += 1) {
-      if (++bitArrayIndex < bitArray.length) {
-        baseImage[i][j][0] = bitArray[bitArrayIndex]
-      }
-      if (++bitArrayIndex < bitArray.length) {
-        baseImage[i][j][1] = bitArray[bitArrayIndex]
-      }
-      if (++bitArrayIndex < bitArray.length) {
-        baseImage[i][j][2] = bitArray[bitArrayIndex]
-      }
-    }
+  if(baseImage.length < message.length){
+    throw new Error('Image is too small');
   }
-
-  return baseImage;
+  const imageToModified = [...baseImage];
+  const bitArray = message.split('').map(v => +v).forEach((v, index)=>{
+    imageToModified[index] += v;
+  })
+  return imageToModified;
 }
 
+function retrieveMessageInImage(baseImage, encodedImage) {
+  if(baseImage.length !== encodedImage.length){
+    throw new Error("Image have not the same length");
+  }
 
+  const result = baseImage.map((value, index)=>{
+    return encodedImage[index] - value;
+  }).join('').match(/(\d{8})/g);
+
+  return result.filter(value=>value !== '00000000').join('');
+}
